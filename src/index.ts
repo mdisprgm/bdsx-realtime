5bce2441dba479ac4ca19f0b931ea03ae2ea08ff
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2021-10-24 19:29:29 +0900

    my update (command to toggle)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
-import { events } from 'bdsx/event';
-import { latitude, longitude } from '../config.json';
+import { events } from "bdsx/event";
+import { latitude, longitude } from "../config.json";
+import fs = require("fs");
+
+export const REALTIME_TXT = "./realtime.txt";
+if (!fs.existsSync(REALTIME_TXT)) {
+    fs.writeFileSync(REALTIME_TXT, "true");
+}
 
 events.serverOpen.on(() => {
     if (!latitude) return console.log(new Error(`latitude is not set!`));
     if (!longitude) return console.log(new Error(`longitude is not set!`));
-    import(`./setTime`)
+    import(`./setTime`);
 });
--- src/setTime.ts
+++ src/setTime.ts
@@ -1,15 +1,56 @@
-import { serverInstance } from 'bdsx/bds/server';
-import { events } from 'bdsx/event';
-import { bedrockServer } from 'bdsx/launcher';
-import { timeAdapter } from './timeAdapter';
+import { serverInstance } from "bdsx/bds/server";
+import { events } from "bdsx/event";
+import { bedrockServer } from "bdsx/launcher";
+import { timeAdapter } from "./timeAdapter";
+import fs = require("fs");
+import { REALTIME_TXT } from ".";
+import { command } from "bdsx/command";
+import { CommandPermissionLevel } from "bdsx/bds/command";
 
-bedrockServer.executeCommand(`gamerule dodaylightcycle false`)
-serverInstance.minecraft.getLevel().setTime(0)
-
-timeAdapter.updateTimeAdapter()
-const tick = timeAdapter.getCurrentTick()
-if (timeAdapter.lastTick !== tick) {
-    timeAdapter.lastTick = tick
-    const setTime = setInterval(() => { serverInstance.minecraft.getLevel().setTime(tick) }, 100)
-    events.serverStop.on(() => { clearInterval(setTime) })
+function isTrue(s: string) {
+    return s !== "false";
 }
+const level = serverInstance.minecraft.getLevel();
+
+const setTime = setInterval(() => {
+    const enabled = fs.readFileSync(REALTIME_TXT, "utf8");
+    if (isTrue(enabled)) {
+        level.setTime(0);
+        timeAdapter.updateTimeAdapter();
+        const tick = timeAdapter.getCurrentTick();
+        if (timeAdapter.lastTick !== tick) {
+            timeAdapter.lastTick = tick;
+        }
+        level.setTime(tick);
+    }
+}, 100);
+events.serverStop.on(() => {
+    clearInterval(setTime);
+});
+
+command
+    .register("realtime", "realtime on/off", CommandPermissionLevel.Operator)
+    .overload(
+        async (p, o, op) => {
+            switch (p.swt) {
+                case "on":
+                    bedrockServer.executeCommand(
+                        `gamerule dodaylightcycle false`
+                    );
+                    fs.writeFileSync(REALTIME_TXT, "true");
+                    op.success("Realtime is §aenabled");
+                    break;
+                case "off":
+                    bedrockServer.executeCommand(
+                        `gamerule dodaylightcycle true`
+                    );
+                    fs.writeFileSync(REALTIME_TXT, "false");
+                    op.success("Realtime is §cdisabled");
+                    level.setTime(500);
+                    break;
+            }
+        },
+        {
+            swt: command.enum("RealTimeSwitch", "on", "off"),
+        }
+    );
